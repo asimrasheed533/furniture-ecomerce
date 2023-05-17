311f23647f4a30022055e5cdd8a82bd58e2506a9
Asim Rasheed <asimrasheed442@gmail.com>
2023-05-17 10:55:32 +0500

    fixs

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import mix from "vite-plugin-mix";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
